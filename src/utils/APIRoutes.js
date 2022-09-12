b127f2b9653ab2500be0d20f26146c35a62dd844
dosorio55 <dosorio55@gmail.com>
2022-09-13 01:24:36 +0200

    resolved a problem with routes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 // export const HOST = "http://localhost:5000";
-const HOST = "https://chat-app-server-do.herokuapp.com";
+export const HOST = "https://chat-app-server-do.herokuapp.com";
 
 export const registerRoute = `${HOST}/user/register`;
 export const loginRoute = `${HOST}/user/login`;
